666f54d388b0b60ce51247e08d267e91a1d73d4f
Delower <alexsey2000@gmail.com>
2021-05-19 20:02:36 +0300

    new wave (this true)

--- services/parsers/parser.py
+++ services/parsers/parser.py
@@ -14,5 +14,3 @@ for i in range(len(n)):
         new_n.append(n[i].text.replace(u'\xa0',' ').replace(u'\u2009', ''))
         for i in range(len(new_n)):
             new_n1 += new_n[i]
-
-print(new_n1)
